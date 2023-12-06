7cf57ea9e1d50b5951bdad8a3886c060b9277faa
Dawid-Stankowski <dawid441@interia.pl>
2023-12-06 07:41:28 +0100

    1.0

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,49 @@
-package PACKAGE_NAME;public class Student {
+public class Student {
+    private String imie;
+    private String nazwisko;
+    private String numerIndeksu;
+
+    // Konstruktor
+    public Student(String imie, String nazwisko, String numerIndeksu) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        setNumerIndeksu(numerIndeksu);
+    }
+
+    public String getImie() {
+        return imie;
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public String getNumerIndeksu() {
+        return numerIndeksu;
+    }
+
+    public void setImie(String imie) {
+        this.imie = imie;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public void setNumerIndeksu(String numerIndeksu) {
+        //Sprawdzamy, czy numer indeksu składa się z samych cyfr
+        if (numerIndeksu.matches("\\d+")) {
+            this.numerIndeksu = numerIndeksu;
+        } else {
+            System.out.println("Błąd: Numer indeksu powinien składać się z samych cyfr.");
+        }
+    }
+
+    public static void main(String[] args) {
+        // Przykład użycia
+        Student student = new Student("Jan", "Kowalski", "12345");
+        System.out.println("Imię: " + student.getImie());
+        System.out.println("Nazwisko: " + student.getNazwisko());
+        System.out.println("Numer indeksu: " + student.getNumerIndeksu());
+    }
 }
